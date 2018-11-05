c8b5976d430942b63505da6120082ace8e312942
redhat <redhanoormansyah30@gmail.com>
2018-11-05 23:22:10 +0700

    Templating Blog Home Page and About 30%

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,9 +2,29 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Redha Noormansyah',
+        'title': 'Blog Post 1',
+        'content': 'Konten Kesatu ini Masih dalam dump variabel belum di simpan di Database',
+        'date_posted': '12 September 2018'
+    },
 
+    {
+        'author': 'John Noormansyah',
+        'title': 'Blog Post 2',
+        'content': 'Konten Kedua ini Masih dalam dump variabel belum di simpan di Database',
+        'date_posted': '25 September 2018'
+    }
+]
 def home(request):
-    return HttpResponse('Blog Home')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('About Page')
+    return render(request, 'blog/about.html',{'title':'About'})
+
+
+
